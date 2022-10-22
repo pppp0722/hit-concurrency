21a8b2f554db951ee11ee1bd79c869694d0a6b63
ilhwan <poj0722@naver.com>
2022-10-22 18:15:24 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/pppp0722/hitconcurrency/HitConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.pppp0722.hitconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pppp0722/hitconcurrency/HitConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pppp0722.hitconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
